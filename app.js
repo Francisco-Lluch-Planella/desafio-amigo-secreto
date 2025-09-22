15f6f71c11d593e5d7750e69db6d37654d0f3c13
Francisco Lluch Planella <franciscolluchplanella@gmail.com>
2025-09-21 22:50:28 -0300

    Implementación de la función para agregar amigos con validación de una entrada vacía en el campo "Escribe un nombre" y agregando un nombre digitado en el campo "Escribe un nombre" al array

--- app.js
+++ app.js
@@ -2,3 +2,20 @@
 
 // Creación de un array para almacenar los nombres de amigos ingresados
 let amigos = [];
+
+// Implementación de la función para agregar amigos
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    // Validación de una entrada vacía en el campo "Escribe un nombre"
+    if (nombre === "") {
+        alert("Por favor, inserta el nombre de un amigo(a).");
+        return;
+    }
+
+    // Agregando un nombre digitado en el campo "Escribe un nombre" al array
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
